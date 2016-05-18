d696b046253c9588ebdf57f567ce93c052300a8a
Jawad <jawadnmd@gmail.com>
2016-05-18 19:53:29 -0400

    gc_respond now responds with controller data
    For both probe commands, gc_respond now sends back the controller data
    
    Issues:
    * Sending the same exact response (current gc_status data) for both
    commands, but console still sends initialization probe command, so
    probably need a different response. Probably will create yet another
    function just to send the initialization command and read immediately
    from the controller afterwards (basically, combine gc_read and gc_send
    once again)
    * Response on console is different from what I thought I was sending.
    Will need to look into that for sure

--- Currnet (Uno) Code/GC_Reader/GC_Reader.ino
+++ Currnet (Uno) Code/GC_Reader/GC_Reader.ino
@@ -442,6 +442,9 @@ static int gc_respond() {
     // Return value is number of bits read
     uint8_t retval;
 
+    uint8_t* respond_buffer = (uint8_t*) &gc_status.data1;
+    uint8_t length_in_bytes = 8;
+
     asm volatile (
             "; START OF MANUAL ASSEMBLY BLOCK\n"
             // default exit value is 0 (nothing valuable read)
@@ -740,9 +743,8 @@ static int gc_respond() {
 
             // TODO: Success, found read probe command! Now, respond!
             "L%=_respond_A:\n"
-            // For now, just return a 1 for success
             "ldi %[retval],lo8(1)\n"
-            "rjmp L%=_exit\n" 
+            "rjmp L%=_respond_base\n" 
 
             // *************************************************************
             "L%=_setup_B3:\n"
@@ -847,9 +849,128 @@ static int gc_respond() {
 
             // TODO: Success, found initialization probe command! Now, respond!
             "L%=_respond_B:\n"
-            // For now, just return a 2 for success
             "ldi %[retval],lo8(2)\n"
-            "rjmp L%=_exit\n" 
+            "rjmp L%=_respond_base\n" 
+
+
+            //==================================================================
+
+            "L%=_respond_base:\n"
+            // passed in to this block are:
+            // the Z register (r31:r30) is the buffer pointer
+            // %[length_in_bytes] is the register holding the length of the buffer in bytes
+
+            // Instruction cycles are noted in parentheses
+            // branch instructions have two values, one if the branch isn't
+            // taken and one if it is
+
+            // r25 will be the current buffer byte loaded from memory
+            // r24 will be the bit counter for the current byte. when this
+            // reaches 0, we need to decrement the length counter, load
+            // the next buffer byte, and loop. (if the length counter becomes
+            // 0, that's our exit condition)
+            
+            "ld r25, Z\n" // load the first byte
+
+            // This label starts the outer loop, which sends a single byte
+            ".L%=_byte_loop:\n"
+            "ldi r24,lo8(8)\n" // (1)
+
+            // This label starts the inner loop, which sends a single bit
+            ".L%=_bit_loop:\n"
+            "sbi 0xa,2\n" // (2) pull the line low
+
+            // line needs to stay low for 1µs for a 1 bit, 3µs for a 0 bit
+            // this block figures out if the next bit is a 0 or a 1
+            // the strategy here is to shift the register left, then test and
+            // branch on the carry flag
+            "lsl r25\n" // (1) shift left. MSB goes into carry bit of status reg
+            "brcc .L%=_zero_bit\n" // (1/2) branch if carry is cleared
+
+            
+            // this block is the timing for a 1 bit (1µs low, 3µs high)
+            // Stay low for 16 - 2 (above lsl,brcc) - 2 (below cbi) = 12 cycles
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\n" // (2)
+            "cbi 0xa,2\n" // (2) set the line high again
+            // Now stay high for 2µs of the 3µs to sync up with the branch below
+            // 2*16 - 2 (for the rjmp) = 30 cycles
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "rjmp .L%=_finish_bit\n" // (2)
+
+
+            // this block is the timing for a 0 bit (3µs low, 1µs high)
+            // Need to go high in 3*16 - 3 (above lsl,brcc) - 2 (below cbi) = 43 cycles
+            ".L%=_zero_bit:\n"
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\n" // (3)
+            "cbi 0xa,2\n" // (2) set the line high again
+
+
+            // The two branches meet up here.
+            // We are now *exactly* 3µs into the sending of a bit, and the line
+            // is high again. We have 1µs to do the looping and iteration
+            // logic.
+            ".L%=_finish_bit:\n"
+            "subi r24,1\n" // (1) subtract 1 from our bit counter
+            "breq .L%=_load_next_byte\n" // (1/2) branch if we've sent all the bits of this byte
+
+            // At this point, we have more bits to send in this byte, but the
+            // line must remain high for another 1µs (minus the above
+            // instructions and the jump below and the sbi instruction at the
+            // top of the loop)
+            // 16 - 2(above) - 2 (rjmp below) - 2 (sbi after jump) = 10
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "rjmp .L%=_bit_loop\n"
+
+
+            // This block starts 3 cycles into the last 1µs of the line being high
+            // We need to decrement the byte counter. If it's 0, that's our exit condition.
+            // If not we need to load the next byte and go to the top of the byte loop
+            ".L%=_load_next_byte:\n"
+            "subi %[length_in_bytes], 1\n" // (1)
+            "breq .L%=_loop_exit\n" // (1/2) if the byte counter is 0, exit
+            "adiw r30,1\n" // (2) increment byte pointer
+            "ld r25, Z\n" // (2) load the next byte
+            // delay block:
+            // needs to go high after 1µs or 16 cycles
+            // 16 - 9 (above) - 2 (the jump itself) - 3 (after jump) = 2
+            "nop\nnop\n" // (2)
+            "rjmp .L%=_byte_loop\n" // (2)
+
+
+            // Loop exit
+            ".L%=_loop_exit:\n"
+
+            // final task: send the stop bit, which is a 1 (1µs low 3µs high)
+            // the line goes low in:
+            // 16 - 6 (above since line went high) - 2 (sbi instruction below) = 8 cycles
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\n" // (3)
+            "sbi 0xa,2\n" // (2) pull the line low
+            // stay low for 1µs
+            // 16 - 2 (below cbi) = 14
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\nnop\n" // (5)
+            "nop\nnop\nnop\nnop\n" // (4)
+            "cbi 0xa,2\n" // (2) set the line high again
+
+            // just stay high. no need to wait 3µs before returning
+            
 
 
  
@@ -857,13 +978,16 @@ static int gc_respond() {
             
             ";END OF MANUAL ASSEMBLY BLOCK\n"
             // ----------
-            // outputs:
-            : [retval] "=r" (retval)
+            : 
+              // outputs:
+              "+z" (respond_buffer), // (read and write)
+              [retval] "=r" (retval)
+            :
+              // inputs:
+              [length_in_bytes] "r" (length_in_bytes)
             // clobbers (registers we use in the assembly for the compiler to
             // avoid):
-              // r24 = scratch register (ex: timeout down counter)
-              // r25 = down counter for reading sequence of bits
-            :: "r25", "r24"         
+            : "r25", "r24"         
             );
 
     return retval;
